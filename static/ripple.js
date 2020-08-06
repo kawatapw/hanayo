17342b5dcbe81bae32ffee4067fb6d30ce218d15
Enzo <me@r33.space>
2020-08-06 22:40:09 +0200

    Remove ripple blog

--- static/ripple.js
+++ static/ripple.js
@@ -896,7 +896,7 @@ function T(s, settings) {
 
 var apiPrivileges = [
   "ReadConfidential", "Write", "ManageBadges", "BetaKeys", "ManageSettings",
-  "ViewUserAdvanced", "ManageUser", "ManageRoles", "ManageAPIKeys", "Blog",
+  "ViewUserAdvanced", "ManageUser", "ManageRoles", "ManageAPIKeys",
   "APIMeta", "Beatmap", "Bancho"
 ];
 
